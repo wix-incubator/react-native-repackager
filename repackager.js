0382c6433ee01f6be7e40ba58a93a348699d0329
Daniel Zlotin <danielzl@wix.com>
2017-06-28 18:21:07 +0300

    working and gracefully failing

--- repackager.js
+++ repackager.js
@@ -13,16 +13,26 @@ const shouldSetup = _.includes(process.argv, 'setup');
 run();
 
 function run() {
+  console.log(`rootDir: ${rootDir}`);
+  console.log(`scriptDir: ${scriptDir}`);
   assertRN44();
   if (shouldSetup) {
+    if (alreadyApplied()) {
+      console.log(`repackager was already applied successfully, exiting`);
+      return;
+    }
     console.log(`injecting support for --customExtensions`);
-    exec.execSync(`git apply --verbose --no-index --directory ${reactNativeDir} ${scriptDir}/rn44PackagerCustomExtensions.patch`);
+    exec.execSync(`git apply --verbose --no-index --directory node_modules/react-native ${scriptDir}/rn44PackagerCustomExtensions.patch`);
   }
 }
 
 function assertRN44() {
   const rnPackageJson = JSON.parse(fs.readFileSync(`${reactNativeDir}/package.json`));
-  if (!_.startsWith(rnPackageJson, '0.44')) {
+  if (!_.startsWith(rnPackageJson.version, '0.44')) {
     throw new Error(`Only react-native 0.44.x is supported currently`);
   }
+}
+
+function alreadyApplied() {
+  return _.includes(String(fs.readFileSync(`${reactNativeDir}/local-cli/cli.js`)), 'repackager applied successfully');
 }
