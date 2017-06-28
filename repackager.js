7e06ce8c081968f960519d139633670f8dda10d1
Daniel Zlotin <danielzl@wix.com>
2017-06-28 19:29:30 +0300

    supporting release source maps and mocked e2e, supporting --reverse

--- repackager.js
+++ repackager.js
@@ -8,11 +8,29 @@ const rootDir = process.cwd();
 const scriptDir = __dirname;
 const reactNativeDir = `${rootDir}/node_modules/react-native`;
 
+const shouldSetup = _.includes(process.argv, 'setup');
+const shouldInjectSourceMap = _.includes(process.argv, 'injectReleaseSourceMap');
+const shouldInjectMockedE2E = _.includes(process.argv, 'injectReleaseMockedE2E');
+
+const shouldReverse = _.includes(process.argv, '--reverse');
+
 run();
 
 function run() {
   assertRN44();
-  if (alreadyApplied()) {
+  if (shouldSetup) {
+    setup();
+  }
+  if (shouldInjectSourceMap) {
+    injectSourceMap();
+  }
+  if (shouldInjectMockedE2E) {
+    injectMockedE2E();
+  }
+}
+
+function setup() {
+  if (alreadySetup()) {
     console.log(`repackager was already applied successfully, exiting`);
     return;
   }
@@ -20,13 +38,27 @@ function run() {
   exec.execSync(`git apply --verbose --no-index --directory node_modules/react-native ${scriptDir}/rn44PackagerCustomExtensions.patch`);
 }
 
+function injectSourceMap() {
+  console.log(`${shouldReverse ? 'reversing' : 'injecting'} bundle sourcemap arg to release builds`);
+  exec.execSync(`git apply ${shouldReverse ? '--reverse' : ''} --verbose --no-index --directory node_modules/react-native ${scriptDir}/rn44PackagerReleaseSourceMap.patch`);
+}
+
+function injectMockedE2E() {
+  console.log(`${shouldReverse ? 'reversing' : 'injecting'} bundle customExtensions=e2e to release builds`);
+  exec.execSync(`git apply ${shouldReverse ? '--reverse' : ''} --verbose --no-index --directory node_modules/react-native ${scriptDir}/rn44PackagerReleaseMockedE2E.patch`);
+}
+
 function assertRN44() {
-  const rnPackageJson = JSON.parse(fs.readFileSync(`${reactNativeDir}/package.json`));
+  const rnPackageFile = `${reactNativeDir}/package.json`;
+  if (!fs.existsSync(rnPackageFile)) {
+    throw new Error(`Can't locate react-native folder in ${reactNativeDir}`);
+  }
+  const rnPackageJson = JSON.parse(fs.readFileSync(rnPackageFile));
   if (!_.startsWith(rnPackageJson.version, '0.44')) {
     throw new Error(`Only react-native 0.44.x is supported currently`);
   }
 }
 
-function alreadyApplied() {
+function alreadySetup() {
   return _.includes(String(fs.readFileSync(`${reactNativeDir}/local-cli/cli.js`)), 'repackager applied successfully');
 }
