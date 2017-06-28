a39e036d3eda2cab7d375fa75339b2e3df69e90a
Daniel Zlotin <danielzl@wix.com>
2017-06-28 16:52:40 +0300

    not using postinstall anymore

--- repackager.js
+++ repackager.js
@@ -1,7 +1,12 @@
+#!/usr/bin/env node
+
 const cp = require('child_process');
 
 run();
 
 function run() {
+  console.log(`applying repackager patch to react-native 0.44`);
+  console.log(`support for --customExtensions`);
+  console.log(`${process.cwd()}`);
   cp.execSync(`git apply --verbose --no-index --directory ../../node_modules/react-native rn44PackagerCustomExtensions.patch`);
 }
